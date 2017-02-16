9d3d20493cc8ef707cb716b06340298c702ae8c4
alpine2sports <sethsykorabodie@gmail.com>
2017-02-16 10:51:10 -0500

    Expanded Date Range
    This allows users to access data all the way up to 2015. It also fixes a
    few bugs and streamlines some sections

--- Scripts/NSIDCDataGrab.py
+++ Scripts/NSIDCDataGrab.py
@@ -1,23 +1,20 @@
-##----------------------------------------------------------------------
+## ---------------------------------------------------------------------------
 ## NSIDCDataGrab.py
 ##
 ## Downloads the NSIDC sea ice concentration binary files for processing.
 ## Data are stored on the National Sea Ice Data Center (NSIDC) ftp site.
 ##
-## Created: Nov 2016
+## Created: Nov 2016 - Feb 2017
 ## Author: Seth Sykora-Bodie - seth.sykora.bodie@duke.edu (for ENV859)
 ##      With help and advice from Corrie Curtis, Ginny Crothers and John Fay
-##----------------------------------------------------------------------
+## ---------------------------------------------------------------------------
 
-# Section #1
-
-#Import modules that will be needed for the analysis
+# Section #1 Import modules that will be needed for the analysis
 print "Importing Python modules"
 import sys, os, urllib, datetime, glob, arcpy
+print "  "
 
 #Get folders  via relative paths
-
-print "  "
 print "Finding relative folder pathways"
 print arcpy.AddMessage("Finding relative folder pathways")
 
@@ -26,17 +23,18 @@ rootFldr = os.path.dirname(scriptFldr)
 dataFldr = os.path.join(rootFldr,"Data")
 scratchFldr = os.path.join(rootFldr, "Scratch")
 dailyFldr = os.path.join(dataFldr, "DailySSMIFiles")
-if not os.path.exists(dailyFldr): os.mkdir(dailyFldr)
-        #remember to add the new Template.txt header file or it won't know how to build the new tiffs later!
+if not os.path.exists(dailyFldr): os.mkdir(dailyFldr) #remember to add the new Template.txt header file or it won't know how to build the new tiffs later!
 tiffFldr = os.path.join(dataFldr,"TiffFiles")  
 if not os.path.exists(tiffFldr): os.mkdir(tiffFldr)
+tiffPercentFldr = os.path.join(dataFldr,"TiffPercent")
+if not os.path.exists(tiffPercentFldr): os.mkdir(tiffPercentFldr)
 
 print "  "
 print "Successfully: imported sys, os, urllib, datetime, glob, arcpy; found file pathways; and created data new folders."
 print arcpy.AddMessage("Successfully: imported sys, os, urllib, datetime, glob, arcpy; found file pathways; and created data new folders.")
 print "  "
 
-# Monthly data sources
+# Set links to monthly data sources
 monthlyURL = 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0051_gsfc_nasateam_seaice/final-gsfc/south/monthly/'
 monthlyFN = 'nt_197811_n07_v1.1_s.bin'     
 
@@ -44,18 +42,17 @@ monthlyFN = 'nt_197811_n07_v1.1_s.bin'
 dailyURL = 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0051_gsfc_nasateam_seaice/final-gsfc/south/daily/'
 dailyFN = 'nt_19790102_n07_v1.1_s.bin'
 
-print "Starting to download daily SSMI data from the National Sea and Ice Data Center"
-print arcpy.AddMessage("Starting to download daily SSMI data from the National Sea and Ice Data Center")
-
 ## ---------------------------------------------------------------------------
 
-# Section #1 Loop through and download the data from 1979 - 1980
+# Section #2 Create user input for an annual range
 
 # Build sys.argv to allow user to input the year
-inputStart = sys.argv[1]          
+inputStart = sys.argv[1]
+    #inputStart = 2011
 inputStartYr = int(inputStart)
-inputStop = sys.argv[2]     
-inputStopYr = int(inputStop)
+inputStop = sys.argv[2]
+    #inputStop = 2012
+inputStopYr = int(inputStop) + 1
 
 # Compile range
 AllYears = range(inputStartYr,inputStopYr)     
@@ -63,38 +60,81 @@ AllYears = range(inputStartYr,inputStopYr)
 # Create else/if statement so that the correct starting day is selected for each year
 if inputStartYr == 1979 or inputStartYr == 1982 or inputStartYr == 1984 or inputStartYr == 1985 or inputStartYr == 1987:
     inputStartDay = 02
-elif inputStartYr == 1980 or inputStartYr == 1981 or inputStartYr == 1983 or inputStartYr == 1986:
+elif inputStartYr == 1980 or inputStartYr == 1981 or inputStartYr == 1983 or inputStartYr == 1986 or inputStartYr == 1989 or inputStartYr == 1990 or inputStartYr == 1991 or inputStartYr == 1992 or inputStartYr == 1993 or inputStartYr == 1994 or inputStartYr == 1995 or inputStartYr == 1996 or inputStartYr == 1997 or inputStartYr == 1998 or inputStartYr == 1999 or inputStartYr == 2000 or inputStartYr == 2001 or inputStartYr == 2002 or inputStartYr == 2003 or inputStartYr == 2004 or inputStartYr == 2005 or inputStartYr == 2006 or inputStartYr == 2007 or inputStartYr == 2008 or inputStartYr == 2009 or inputStartYr == 2010 or inputStartYr == 2011 or inputStartYr == 2012 or inputStartYr == 2013 or inputStartYr == 2014 or inputStartYr == 2015:
     inputStartDay = 01
+elif inputStartYr == 1988:
+    inputStartDay = 13
+
+## ---------------------------------------------------------------------------
+
+# Section #3 Loop through and download the data from 1979 - 2015
+
+print "Starting to download daily SSMI data from the National Sea and Ice Data Center"
+print arcpy.AddMessage("Starting to download daily SSMI data from the National Sea and Ice Data Center")
 
 # Parse dates
 d = datetime.date(inputStartYr,01,inputStartDay)       
-td = datetime.timedelta(days=2)
+td = datetime.timedelta(days=1)
 nextDay = d                           
 
 # Download files inside a loop
 for year in AllYears:
     while nextDay.year == year:
-        yr = str(nextDay.year)
-        mnth = str(nextDay.month)
-        mnth = str(mnth).zfill(2)
-        dy = str(nextDay.day)
-        dy = str(dy).zfill(2)
+        # Build in a line for exceptions if the ftp link doesn't exist
+        try:
+            yr = str(nextDay.year)
+            mnth = str(nextDay.month)
+            mnth = str(mnth).zfill(2)
+            dy = str(nextDay.day)
+            dy = str(dy).zfill(2)
 
-        #Build the new url name
-        dailyFN = 'nt_' + yr + mnth + dy + '_n07_v1.1_s.bin'
-        NewURL = dailyURL + yr + '/' + dailyFN 
+            #Build the new date
+            date = datetime.date(int(yr), int(mnth), int(dy))
+            startn07 = datetime.date(1979,1,2)
+            endn07 = datetime.date(1987,8,20)
 
-        #Create the output file
-        outFN = os.path.join(dailyFldr,dailyFN)
-        if os.path.exists(outFN):
-            print "-->{} exists; skipping".format(outFN)
-            print arcpy.AddMessage("-->{} exists; skipping".format(outFN))
-            
-        #Write to the local file
-        urllib.urlretrieve(NewURL,outFN)
-        print "downloading and writing file: " + dailyFN
-        print arcpy.AddMessage("downloading and writing file: " + dailyFN)
-        
+            startf08 = datetime.date(1987,8,21)
+            endf08 = datetime.date(1991,12,18)
+
+            startf11 = datetime.date(1991,12,19)
+            endf11 = datetime.date(1995,9,29)
+
+            startf13 = datetime.date(1995,9,30)
+            endf13 = datetime.date(2007,12,31)
+
+            startf17 = datetime.date(2008,1,1)
+
+            #Set the sensor type
+            if date >= startn07 and date <= endn07:
+                sens = 'n07'
+            elif date >= startf08 and date <= endf08:
+                sens = 'f08'
+            elif date >= startf11 and date <= endf11:
+                sens = 'f11'
+            elif date >= startf13 and date <= endf11:
+                sens = 'f13'
+            elif date >= startf17:
+                sens = 'f17'
+
+            #Build the new url name
+            dailyFN = 'nt_' + yr + mnth + dy + '_n07_v1.1_s.bin'
+            NewURL = dailyURL + yr + '/' + dailyFN 
+
+            #Create the output file
+            outFN = os.path.join(dailyFldr,dailyFN)
+            if os.path.exists(outFN):
+                print "-->{} exists; skipping".format(outFN)
+                print arcpy.AddMessage("-->{} exists; skipping".format(outFN))
+                
+            #Write to the local file
+            urllib.urlretrieve(NewURL,outFN)
+            print "downloading and writing file: " + dailyFN
+            print arcpy.AddMessage("downloading and writing file: " + dailyFN)
+
+        # Close out the ftp exceptions error skip
+        except Exception:
+            pass
+
         #Add days to create new date URL
         nextDay = nextDay + td
 
@@ -107,9 +147,7 @@ print arcpy.AddMessage("The script has completed downloading the sea ice concent
 
 ## ---------------------------------------------------------------------------
 
-# Section #2
-
-# Loop through filenames to change file extensions from .bin to .bil
+# Section #4 Loop through filenames to change file extensions from .bin to .bil
 # and shorten filename to create consistent format across all months and years
         # Original file format from NSIDC for reference
         # 'nt_YYYYMM_n07_v1.1_n.bin'
@@ -128,8 +166,7 @@ print "  "
 
 ## ---------------------------------------------------------------------------
 
-# Section #3
-# Create matching header text files (the conversion to TIFF requires a 
+# Section #5 Create matching header text files (the conversion to TIFF requires a 
 # text file (.hdr) that gives column and row headers for the new file
 
 headerFile = dailyFldr + "\\template.txt"
@@ -152,7 +189,7 @@ print "  "
 
 ## ---------------------------------------------------------------------------
 
-# Section #4
+# Section #6 Convert .bil files to TIFFs
 
 # Set geoprocessing environments
 print "Setting geoprocessing environments"
@@ -167,7 +204,6 @@ print arcpy.AddMessage("Beginning to convert binary files into tiff files")
 print "  "
 
 # Loop through binary files and convert them
-
 for filename in glob.iglob(os.path.join(dailyFldr, '*.bil')):
     arcpy.RasterToOtherFormat_conversion(filename, tiffFldr, "TIFF")
 
@@ -178,7 +214,7 @@ print "  "
 
 ## ---------------------------------------------------------------------------
 
-# Section #5
+# Section #7
 
 # Define projection to custom coordinate system created per NSIDC parameters found here:
 # http://nsidc.org/support/21680984-How-do-I-import-the-0051-sea-ice-concentration-data-into-ArcGIS-
